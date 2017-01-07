31c7de8a4fe1cb09ca9ab5d328b99ef616b51d23
KOMPUTER-KINGI\Kinia <kinga.kurowska3@gmail.com>
2017-01-07 04:11:19 +0100

    Wstępny wstęp

--- programy/porownanie_BH.py
+++ programy/porownanie_BH.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 N = 20 #N
 M = 5 #M
-n = 10 #n
+n = 17 #n
 
 hg = hypergeom(N, M, n)
 p = M/N
@@ -24,25 +24,34 @@ m2 = b.mean()
 v2 = b.var()
 print("b.mean =", m2, "b.var =", v2)
 
-vectorX = np.arange(0, n+1)
-vectorP1 = hg.pmf(vectorX)
-vectorP2 = b.pmf(vectorX)
+L = max(0, M - N + n)
+U = min(n, M)
+
+vectorX1 = np.arange(L, U+1)
+vectorX2 = np.arange(0, n+1)
+
+vectorP1 = hg.pmf(vectorX1)
+vectorP2 = b.pmf(vectorX2)
+
+maxy = 0.5
 
 fig = plt.figure()
-plt.plot(vectorX, vectorP1, 'o')
-plt.vlines(vectorX, 0, vectorP1)
-plt.axis([0, n, 0, 1])
+plt.plot(vectorX1, vectorP1, 'o')
+plt.vlines(vectorX1, 0, vectorP1)
+plt.axis([0, n, 0, maxy])
 plt.grid(True)
 plt.xlabel('$k$', fontsize=14)
 plt.ylabel('$P(X=k)$', fontsize=14)
+plt.title('$X\sim h(17,5,20)$', fontsize=14)
 
 fig = plt.figure()
-plt.plot(vectorX, vectorP2, 'ro')
-plt.vlines(vectorX, 0, vectorP2)
-plt.axis([0, n, 0, 1])
+plt.plot(vectorX2, vectorP2, 'ro')
+plt.vlines(vectorX2, 0, vectorP2)
+plt.axis([0, n, 0, maxy])
 plt.grid(True)
 plt.xlabel('$k$', fontsize=14)
 plt.ylabel('$P(X=k)$', fontsize=14)
+plt.title('$X\sim B(10,0.25)$', fontsize=14)
 
 
 
